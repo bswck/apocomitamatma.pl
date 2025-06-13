dcefe924c07055e7eca80e1533daae5088a5d2b9
bswck <bartosz@slawecki.dev>
2025-06-13 08:05:53 +0200

    Smaller vertical stripes

--- front/src/lib/components/VerticalStripes.svelte
+++ front/src/lib/components/VerticalStripes.svelte
@@ -1,4 +1,4 @@
 <div
-	class="absolute -z-10 h-full w-full bg-[url('$lib/images/decorations/vertical1.gif')] bg-center brightness-[15%]"
-	style="background-size: 21rem;"
+	class="absolute -z-10 h-full w-full bg-[url('$lib/images/decorations/vertical1.gif')] bg-center bg-repeat brightness-[15%]"
+	style="background-size: 5rem;"
 ></div>

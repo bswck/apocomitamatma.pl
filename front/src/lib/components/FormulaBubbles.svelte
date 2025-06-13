752636ed7aff0b358360b56fb2ca43a421d377e8
bswck <bartosz@slawecki.dev>
2025-06-13 07:46:26 +0200

    Scale formulas smaller

--- front/src/lib/components/FormulaBubbles.svelte
+++ front/src/lib/components/FormulaBubbles.svelte
@@ -5,15 +5,15 @@
 	const formulas = Object.values(formulasMap);
 
 	const positions = [
-		{ x: 7, y: 12, scale: 3.2, opacity: 0.22 },
-		{ x: 48, y: 9, scale: 2.9, opacity: 0.28 },
-		{ x: 93, y: 14, scale: 3.5, opacity: 0.32 },
-		{ x: 10, y: 47, scale: 4.1, opacity: 0.18 },
-		{ x: 50, y: 52, scale: 3.7, opacity: 0.35 },
-		{ x: 90, y: 48, scale: 2.8, opacity: 0.25 },
-		{ x: 12, y: 87, scale: 3.0, opacity: 0.19 },
-		{ x: 53, y: 91, scale: 3.3, opacity: 0.3 },
-		{ x: 94, y: 86, scale: 3.6, opacity: 0.22 }
+		{ x: 7, y: 12, scale: 1.2, opacity: 0.22 },
+		{ x: 48, y: 9, scale: 1.9, opacity: 0.28 },
+		{ x: 93, y: 14, scale: 1.5, opacity: 0.32 },
+		{ x: 10, y: 47, scale: 1.1, opacity: 0.18 },
+		{ x: 50, y: 52, scale: 1.7, opacity: 0.35 },
+		{ x: 90, y: 48, scale: 1.8, opacity: 0.25 },
+		{ x: 12, y: 87, scale: 1.0, opacity: 0.19 },
+		{ x: 53, y: 91, scale: 1.3, opacity: 0.3 },
+		{ x: 94, y: 86, scale: 1.6, opacity: 0.22 }
 	];
 
 	let scrollY = 0;

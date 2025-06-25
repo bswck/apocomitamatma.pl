6351d9b9157b6ed03f8c0340ae044315c2964419
bswck <bartosz@slawecki.dev>
2025-06-25 17:06:44 +0200

    Move icons

--- front/src/lib/sections/Hero.svelte
+++ front/src/lib/sections/Hero.svelte
@@ -3,6 +3,8 @@
 	import collab from '$lib/images/collab.png';
 	import Sprite from '$lib/components/Sprite.svelte';
 	import Social from '$lib/components/Social.svelte';
+	import youtubeImg from '$lib/images/icons/youtube.png';
+	import discordImg from '$lib/images/icons/discord.png';
 </script>
 
 <div class="font-display backdrop-brightness-70 relative flex w-full grow-0">
@@ -28,8 +30,8 @@
 				Kolorowa matematyka<br />z humorem
 			</h1>
 			<nav aria-roledescription="Links to socials" class="flex items-center justify-evenly w-1/2">
-				<Social platform="youtube" ><img class="w-16" src="/youtube.png" alt=""></Social>
-				<Social platform="discord" ><img class="w-16" src="/discord.png" alt=""></Social>
+				<Social platform="youtube" ><img class="w-16" src={youtubeImg} alt=""></Social>
+				<Social platform="discord" ><img class="w-16" src={discordImg} alt=""></Social>
 			</nav>
 
 			<div class="flex w-full flex-col items-center justify-evenly gap-2 p-4 lg:flex-row lg:gap-0">

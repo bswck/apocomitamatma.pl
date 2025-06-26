40f4507eaca487cbcedeff24c3c449870c61aae1
bswck <bartosz@slawecki.dev>
2025-06-26 10:32:16 +0200

    Ship minimal

--- front/src/routes/new/Card.svelte
+++ front/src/routes/new/Card.svelte
@@ -2,6 +2,8 @@
 	let { children, class: classes = '' } = $props();
 </script>
 
-<div class="w-full flex-col flex items-center gap-6 {classes}">
-    {@render children()}
+<div
+	class="border-(--color-outline) border-3 flex w-full flex-col items-center gap-6 rounded-2xl p-4 {classes}"
+>
+	{@render children()}
 </div>

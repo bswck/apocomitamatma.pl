0b1c02428c4c2e31517c9991183dfd95c30cf797
bswck <bartosz@slawecki.dev>
2025-06-26 11:24:49 +0200

    Fixups

--- front/src/lib/components/Partners.svelte
+++ front/src/lib/components/Partners.svelte
@@ -9,21 +9,14 @@
 <div class="flex w-full flex-col items-center justify-center gap-4 p-8">
 	<h2 class="text-2xl font-bold">Partnerzy</h2>
 	<div class="flex flex-col items-center justify-center gap-4">
+		<Link url="/showcase" newTab={true}>-> Zobacz przykładowe lokowania</Link>
 		<div class="flex flex-row gap-4">
-			<Link newTab={true} url="https://www.nowaera.pl/">
-				<img src={NowaEra} alt="Nowa Era" class="h-auto w-60 object-cover" />
-			</Link>
-			<Link newTab={true} url="https://pwn.pl/">
-				<img src={PWN} alt="PWN" class="h-auto w-60 object-contain" />
-			</Link>
+			<img src={NowaEra} alt="Nowa Era" class="w-46 h-auto object-cover" />
+			<img src={PWN} alt="PWN" class="w-46 h-auto object-contain" />
 		</div>
 		<div class="flex flex-col gap-4 md:flex-row">
-			<Link newTab={true} url="https://swps.pl/">
-				<img src={SWPS} alt="SWPS" class="h-auto w-60 object-contain" />
-			</Link>
-			<Link newTab={true} url="https://wskz.pl/">
-				<img src={WSKZ} alt="WSKZ" class="h-auto w-60 object-contain" />
-			</Link>
+			<img src={SWPS} alt="SWPS" class="w-46 h-auto object-contain" />
+			<img src={WSKZ} alt="WSKZ" class="w-46 h-auto object-contain" />
 		</div>
 	</div>
 </div>
---
+++ front/src/routes/showcase/+page.svelte
@@ -0,0 +1,73 @@
+<script>
+	import NowaEra from '$lib/images/partners/nowaera.png';
+	import PWN from '$lib/images/partners/pwn.png';
+	import SWPS from '$lib/images/partners/swps.png';
+	import WSKZ from '$lib/images/partners/wskz.png';
+	import Link from '$lib/components/Link.svelte';
+</script>
+
+<div class="mx-auto flex h-min w-full grow flex-col items-center">
+	<div class="mx-auto flex w-full max-w-7xl flex-col items-center gap-8 p-8">
+		<h1 class="text-3xl font-bold">Przykłady lokowania</h1>
+
+		<div class="flex w-full flex-col gap-8">
+			<div class="flex items-center justify-center gap-8">
+				<Link newTab={true} url="https://www.nowaera.pl/">
+					<img src={NowaEra} alt="Nowa Era" class="h-auto w-60 object-cover" /></Link
+				>
+
+				<iframe
+					class="aspect-video w-full"
+					src="https://www.youtube-nocookie.com/embed/Mi710Z_K5Y8"
+					title=""
+					frameborder="0"
+					allow="clipboard-write; encrypted-media; picture-in-picture;"
+					allowfullscreen
+				></iframe>
+			</div>
+			<div class="flex items-center justify-center gap-8">
+				<Link newTab={true} url="https://www.pwn.pl/">
+					<img src={PWN} alt="PWN" class="h-auto w-60 object-contain" />
+				</Link>
+
+				<iframe
+					class="aspect-video w-full"
+					src="https://www.youtube-nocookie.com/embed/MXeGVBp5PCw"
+					title=""
+					frameborder="0"
+					allow="clipboard-write; encrypted-media; picture-in-picture;"
+					allowfullscreen
+				></iframe>
+			</div>
+			<div class="flex items-center justify-center gap-8">
+				<Link newTab={true} url="https://www.swps.pl/">
+					<img src={SWPS} alt="SWPS" class="h-auto w-60 object-contain" />
+				</Link>
+
+				<iframe
+					class="aspect-video w-full"
+					src="https://www.youtube-nocookie.com/embed/EXOUgyFomxs"
+					title=""
+					frameborder="0"
+					allow="clipboard-write; encrypted-media; picture-in-picture;"
+					allowfullscreen
+				></iframe>
+			</div>
+
+			<div class="flex items-center justify-center gap-8">
+				<Link newTab={true} url="https://www.wskz.pl/">
+					<img src={WSKZ} alt="WSKZ" class="h-auto w-60 object-cover" /></Link
+				>
+
+				<iframe
+					class="aspect-video w-full"
+					src="https://www.youtube-nocookie.com/embed/rM9VeXkSVpw"
+					title=""
+					frameborder="0"
+					allow="clipboard-write; encrypted-media; picture-in-picture;"
+					allowfullscreen
+				></iframe>
+			</div>
+		</div>
+	</div>
+</div>
